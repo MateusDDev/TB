d7c69c69dee3ba54b7ef3b5ed45fcbc17e23bfef
MateusDDev <ms4811978@gmail.com>
2024-03-14 11:22:05 -0300

    feat: mais testes

--- app/backend/src/tests/leaderboard.test.ts
+++ app/backend/src/tests/leaderboard.test.ts
@@ -8,9 +8,7 @@ import { app } from '../app';
 import { Response } from 'superagent';
 import leaderboardMock from './mocks/leaderboard.mock';
 import matchMock from './mocks/match.mock';
-import MatchService from '../services/MatchService';
 import SequelizeMatch from '../database/models/SequelizeMatch';
-import LeaderboardService from '../services/LeaderboardService';
 
 chai.use(chaiHttp);
 
@@ -19,12 +17,25 @@ const { expect } = chai;
 describe('Testes da rota leaderboard', () => {
     let chaiHttpResponse: Response;
 
-    it('Testa se retorna uma tabela corretamente', async () => {
+    it('Testa se retorna a classificação de partidas em casa corretamente', async () => {
         sinon.stub(SequelizeMatch, 'findAll').resolves(matchMock.matches as any);
 
         chaiHttpResponse = await chai.request(app).get('/leaderboard/home');
 
         expect(chaiHttpResponse.status).to.be.equal(200);
-        expect(chaiHttpResponse.body).to.be.deep.equal(leaderboardMock.board);
+        expect(chaiHttpResponse.body).to.be.deep.equal(leaderboardMock.homeLeaderboard);
+    })
+
+    it('Testa se retorna a classificação de partidas fora de casa corretamente', async () => {
+        sinon.stub(SequelizeMatch, 'findAll').resolves(matchMock.matches as any);
+
+        chaiHttpResponse = await chai.request(app).get('/leaderboard/away');
+
+        expect(chaiHttpResponse.status).to.be.equal(200);
+        expect(chaiHttpResponse.body).to.be.deep.equal(leaderboardMock.awayLeaderboard);
+    })
+
+    afterEach(() => {
+        sinon.restore();
     })
 })
--- app/backend/src/tests/mocks/leaderboard.mock.ts
+++ app/backend/src/tests/mocks/leaderboard.mock.ts
@@ -1,4 +1,4 @@
-const board = [
+const homeLeaderboard = [
     {
         name: 'Flamengo',
         totalPoints: 3,
@@ -25,6 +25,86 @@ const board = [
     },
 ]
 
+const awayLeaderboard = [
+    {
+        name: 'Grêmio',
+        totalPoints: 1,
+        totalGames: 1,
+        totalVictories: 0,
+        totalDraws: 1,
+        totalLosses: 0,
+        goalsFavor: 1,
+        goalsOwn: 1,
+        goalsBalance: 0,
+        efficiency: 33.33
+    },
+    {
+        name: 'Internacional',
+        totalPoints: 0,
+        totalGames: 1,
+        totalVictories: 0,
+        totalDraws: 0,
+        totalLosses: 1,
+        goalsFavor: 0,
+        goalsOwn: 2,
+        goalsBalance: -2,
+        efficiency: 0,
+    }
+]
+
+const defaultLeadboard = [
+    {
+        name: 'Flamengo',
+        totalPoints: 3,
+        totalGames: 1,
+        totalVictories: 1,
+        totalDraws: 0,
+        totalLosses: 0,
+        goalsFavor: 2,
+        goalsOwn: 0,
+        goalsBalance: 2,
+        efficiency: 100.00
+    },
+    {
+        name: 'São Paulo',
+        totalPoints: 1,
+        totalGames: 1,
+        totalVictories: 0,
+        totalDraws: 1,
+        totalLosses: 0,
+        goalsFavor: 1,
+        goalsOwn: 1,
+        goalsBalance: 0,
+        efficiency: 33.33,
+    },
+    {
+        name: 'Grêmio',
+        totalPoints: 1,
+        totalGames: 1,
+        totalVictories: 0,
+        totalDraws: 1,
+        totalLosses: 0,
+        goalsFavor: 1,
+        goalsOwn: 1,
+        goalsBalance: 0,
+        efficiency: 33.33
+    },
+    {
+        name: 'Internacional',
+        totalPoints: 0,
+        totalGames: 1,
+        totalVictories: 0,
+        totalDraws: 0,
+        totalLosses: 1,
+        goalsFavor: 0,
+        goalsOwn: 2,
+        goalsBalance: -2,
+        efficiency: 0,
+    },
+]
+
 export default {
-    board,
+    homeLeaderboard,
+    awayLeaderboard,
+    defaultLeadboard,
 }
